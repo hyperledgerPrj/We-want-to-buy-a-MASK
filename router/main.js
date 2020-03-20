61ca1c215fd922c7b2beb08998a12a01178f7b74
midum <dndn960321@gmail.com>
2020-03-21 00:16:38 +0900

    master add

--- router/main.js
+++ router/main.js
@@ -1,108 +1,169 @@
-const crypto = require('crypto');
-const session = require('express-session');
-const MySQLStore = require('express-mysql-session')(session);
-const bodyParser = require('body-parser');
-let mysql = require('mysql');
-let dbConfig = require('../dbconfig');
-let conn = mysql.createConnection(dbConfig);
+const crypto = require("crypto");
+const session = require("express-session");
+const MySQLStore = require("express-mysql-session")(session);
+const bodyParser = require("body-parser");
+// var mysql = require('mysql');
+// var dbConfig = require('./dbconfig');
+// var conn = mysql.createConnection(dbOptions);
+// conn.connect();
 
-// db connection
-conn.connect(function(err) {
-  if (err) {
-    return console.error('error: ' + err.message);
-  }
+module.exports = function(app) {
+  app.use(
+    session({
+      secret: "!@#$%^&*",
+      // store: new MySQLStore(dbOptions),
+      resave: false,
+      saveUninitialized: false
+    })
+  );
+
+  app.use(bodyParser.json()); // to support JSON-encoded bodies
+  app.use(
+    bodyParser.urlencoded({
+      // to support URL-encoded bodies
+      extended: true
+    })
+  );
+
+  app.get("/", function(req, res) {
+	  res.redirect("/welcome");
+    //if (!req.session.name) res.redirect("/login");
+    res.render("index");
+  });
+
+   app.get("/welcome", function(req, res) {
+    //if (!req.session.name) return res.redirect("/login");
+    //else res.render("welcome", { name: req.session.name });
+  //	res.redirect("/welcome");
+       res.render("welcome", { message: "input your id and password." });
+    });
+	
+  app.get("/login", function(req, res) {
+    //if (!req.session.name)
+    //  res.render("login", { message: "input your id and password." });
+    //else res.redirect("/welcome");
+	res.render("login");
+  });
+  
+  app.get("/signup", function(req, res) {
+    //if (!req.session.name) return res.redirect("/login");
+    //else res.render("signup.html", { name: req.session.name });
+	res.render("signup", { message: "input your id and password." });
+  });
+  
+   app.get("/indexlogin", function(req, res) {
+    //if (!req.session.name) return res.redirect("/login");
+    //else res.render("signup.html", { name: req.session.name });
+	res.render("indexlogin", { message: "input your id and password." });
+  });
+  
  
-  console.log('Connected to the MySQL server.');
-});
-
-module.exports = function (app) {
-  app.use(session({
-    secret: '!@#$%^&*',
-    store: new MySQLStore(dbConfig),
-    resave: false,
-    saveUninitialized: false
-  }));
-
-  app.use(bodyParser.json());       // to support JSON-encoded bodies
-  app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-    extended: true
-  }));
-
-  app.get('/', function (req, res) {
-    if (!req.session.name)
-      res.redirect('/login');
-    else
-      res.redirect('/welcome');
+ app.get("/index", function(req, res) {
+    //if (!req.session.name) return res.redirect("/login");
+    //else res.render("signup.html", { name: req.session.name });
+	res.render("index", { message: "input your id and password." });
+  });
+  
+  app.get("/registersupplier", function(req, res) {
+    //if (!req.session.name) return res.redirect("/login");
+    //else res.render("signup.html", { name: req.session.name });
+	res.render("[rgis]supplier", { message: "input your id and password." });
+  });
+  app.get("/registerseller", function(req, res) {
+    //if (!req.session.name) return res.redirect("/login");
+    //else res.render("signup.html", { name: req.session.name });
+	res.render("[rgis]seller", { message: "input your id and password." });
+  });
+  app.get("/registerdis", function(req, res) {
+    //if (!req.session.name) return res.redirect("/login");
+    //else res.render("signup.html", { name: req.session.name });
+	res.render("[rgis]distributor", { message: "input your id and password." });
+  });
+  app.get("/dislist", function(req, res) {
+    //if (!req.session.name) return res.redirect("/login");
+    //else res.render("signup.html", { name: req.session.name });
+	res.render("[mask]distributorlist", { message: "input your id and password." });
+  });
+  app.get("/sellerlist", function(req, res) {
+    //if (!req.session.name) return res.redirect("/login");
+    //else res.render("signup.html", { name: req.session.name });
+	res.render("[mask]sellerlist", { message: "input your id and password." });
+  });
+  app.get("/supplierlist", function(req, res) {
+    //if (!req.session.name) return res.redirect("/login");
+    //else res.render("signup.html", { name: req.session.name });
+	res.render("[mask]supplierlist", { message: "input your id and password." });
+  });
+   app.get("/policy_notice", function(req, res) {
+    //if (!req.session.name) return res.redirect("/login");
+    //else res.render("signup.html", { name: req.session.name });
+	res.render("policy_notice", { message: "input your id and password." });
+  });
+   app.get("/policy_notice_login", function(req, res) {
+    //if (!req.session.name) return res.redirect("/login");
+    //else res.render("signup.html", { name: req.session.name });
+	res.render("policy_notice_login", { message: "input your id and password." });
+  });
+   app.get("/sellerdetail", function(req, res) {
+    //if (!req.session.name) return res.redirect("/login");
+    //else res.render("signup.html", { name: req.session.name });
+	res.render("sellerlist_detail", { message: "input your id and password." });
+  });
+  app.get("/supplierdetail", function(req, res) {
+    //if (!req.session.name) return res.redirect("/login");
+    //else res.render("signup.html", { name: req.session.name });
+	res.render("supplier_detail", { message: "input your id and password." });
+  });
+   app.get("/disdetail", function(req, res) {
+    //if (!req.session.name) return res.redirect("/login");
+    //else res.render("signup.html", { name: req.session.name });
+	res.render("distributor_detail", { message: "input your id and password." });
   });
 
-  app.get('/login', function (req, res) {
-    if (!req.session.name)
-      res.render('login', { message: 'input your id and password.' });
-    else
-      res.redirect('/welcome');
-  });
-
-  app.get('/welcome', function (req, res) {
-    if (!req.session.name)
-      return res.redirect('/login');
-    else
-      res.render('welcome', { name: req.session.name });
-  });
-
-  app.get('/logout', function (req, res) {
-    req.session.destroy(function (err) {
-      res.redirect('/');
+  app.get("/logout", function(req, res) {
+    req.session.destroy(function(err) {
+      res.redirect("/");
     });
   });
 
-  app.post('/login', function (req, res) {
-    const id = req.body.username;
-    const password = req.body.password;
+  app.post("/login", function(req, res) {
+    let id = req.body.userid;
+    let password = req.body.password;
 
-    let salt = '';
-    let pw = '';
-
-
-//  let id = req.body.username;
-  let pw = req.body.password;
-  let sql = 'SELECT * FROM user WHERE email=?';
-  conn.query(sql, [email], function (err, results) {
-  
-  if (err)
-   console.log(err);
-   })
-
+    let salt = "";
+    let pw = "";
 
     crypto.randomBytes(64, (err, buf) => {
       if (err) throw err;
-      salt = buf.toString('hex');
+      salt = buf.toString("hex");
     });
 
-    crypto.pbkdf2(password, salt, 100000, 64, 'sha512', (err, derivedKey) => {
+    crypto.pbkdf2(password, salt, 100000, 64, "sha512", (err, derivedKey) => {
       if (err) throw err;
-      pw = derivedKey.toString('hex');
+      pw = derivedKey.toString("hex");
     });
 
-    // let user = results[0];
-    crypto.pbkdf2(password, salt, 100000, 64, 'sha512', function (err, derivedKey) {
-      if (err)
-        console.log(err);
-      if (derivedKey.toString('hex') === pw) {
+    // var user = results[0];
+    crypto.pbkdf2(password, salt, 100000, 64, "sha512", function(
+      err,
+      derivedKey
+    ) {
+      if (err) console.log(err);
+      if (derivedKey.toString("hex") === pw) {
         req.session.name = id;
-        req.session.save(function () {
-          return res.redirect('/welcome');
+        req.session.save(function() {
+          return res.redirect("/welcome");
         });
+      } else {
+        return res.render("login", { message: "please check your password." });
       }
-      else {
-        return res.render('login', { message: 'please check your password.' });
-      }
-    });//pbkdf2
+    }); //pbkdf2
   }); // end of app.post
 
   // app.post('/login', function (req, res) {
-  // let id = req.body.username;
-  // let pw = req.body.password;
-  // let sql = 'SELECT * FROM user WHERE id=?';
+  // var id = req.body.userid;
+  // var pw = req.body.password;
+  // var sql = 'SELECT * FROM user WHERE id=?';
   // conn.query(sql, [id], function (err, results) {
   // if (err)
   // console.log(err);
@@ -110,7 +171,7 @@ module.exports = function (app) {
   // if (!results[0])
   // return res.render('login', { message: 'please check your id.' });
 
-  // let user = results[0];
+  // var user = results[0];
   // crypto.pbkdf2(pw, user.salt, 100000, 64, 'sha512', function (err, derivedKey) {
   // if (err)
   // console.log(err);
@@ -126,4 +187,4 @@ module.exports = function (app) {
   // });//pbkdf2
   // });//query
   // });
-}
+};
